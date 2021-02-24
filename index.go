8efabed35d67ad840015e315a3915eb3c74c89be
“xiangwang” <344496982@qq.com>
2021-02-24 17:34:39 +0800

    tijiao

--- index.go
+++ index.go
@@ -3,26 +3,12 @@ package main
 import (
 	"fmt"
 	"github.com/unknwon/goconfig"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
 	initDb()
-	//// 1.创建路由
-	//// 默认使用了2个中间件Logger(), Recovery()
-	//r := gin.Default()
-	//// 路由组1 ，处理GET请求
-	//v1 := r.Group("/v1")
-	//// {} 是书写规范
-	//{
-	//	v1.GET("/login", login)
-	//	v1.GET("submit", submit)
-	//}
-	//v2 := r.Group("/v2")
-	//{
-	//	v2.POST("/login", login)
-	//	v2.POST("/submit", submit)
-	//}
-	//r.Run(":8000")
 }
 
 func initDb() {
@@ -30,25 +16,35 @@ func initDb() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	db_host, err := cfg.GetValue("", "DB_HOST")
+	//db_host, err := cfg.GetValue("", "DB_HOST")
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	db_connection, err := cfg.GetValue("", "DB_CONNECTION")
 	if err != nil {
 		fmt.Println(err)
 	}
-	db_port, err := cfg.GetValue("", "DB_PORT")
-	if err != nil {
-		fmt.Println(err)
+	//db_port, err := cfg.GetValue("", "DB_PORT")
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//db_database, err := cfg.GetValue("", "DB_DATABASE")
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//db_username, err := cfg.GetValue("", "DB_USERNAME")
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//db_password, err := cfg.GetValue("", "DB_PASSWORD")
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	DB, err := gorm.Open(sqlite.Open(db_connection), &gorm.Config{})
+	if err == nil {
+		fmt.Println("open db sucess", DB)
+	} else {
+		fmt.Println("open db error ", err)
 	}
-	db_database, err := cfg.GetValue("", "DB_DATABASE")
-	if err != nil {
-		fmt.Println(err)
-	}
-	db_username, err := cfg.GetValue("", "DB_USERNAME")
-	if err != nil {
-		fmt.Println(err)
-	}
-	db_password, err := cfg.GetValue("", "DB_PASSWORD")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(db_host, db_port, db_database, db_username, db_password)
+
 }
