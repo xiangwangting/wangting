fb668e785da6f9dc6d52b78ef20556034182e0e6
“xiangwang” <344496982@qq.com>
2021-02-25 15:56:15 +0800

    tijiao

--- routes/routes.go
+++ routes/routes.go
@@ -2,10 +2,17 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	"wangting/app/conrtoller/demoController"
+	"github.com/syyongx/php2go"
 )
 
 func Load(router *gin.Engine)  {
 	api := router.Group("api/v1")
-	api.GET("app/*any", demoController.Any)
+	api.GET("/*any",handel)
+}
+
+func handel(c *gin.Context){
+	path := c.Request.URL.Path
+	path = php2go.StrReplace("/api/v1/","",path,9999)
+	routePackage := php2go.Explode("/",path)[0]
+	c.String(200, routePackage)
 }
