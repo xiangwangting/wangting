0567cf10b12eba9496028523b8f38fa6cc8b939c
“xiangwang” <344496982@qq.com>
2021-02-25 22:10:09 +0800

    tijiao

--- http/route/demoRoute/demoRoute.go
+++ http/route/demoRoute/demoRoute.go
@@ -1,1 +1,5 @@
 package demoRoute
+
+func demofunc(){
+
+}
--- http/route/route.go
+++ http/route/route.go
@@ -6,14 +6,27 @@ import (
 )
 
 //加载路由
-func Load(router *gin.Engine)  {
-	api := router.Group("api/v1")
-	api.GET("/*any",handel)
+func Load(router *gin.Engine) {
+	initApiRoute(router)
+
+	router.GET("/",handel)
 }
 
-func handel(c *gin.Context){
+func initApiRoute(router *gin.Engine) {
+	api := router.Group("api/v1")
+	api.GET("/*any", handel)
+	api.POST("/*any", handel)
+	api.PUT("/*any", handel)
+	api.DELETE("/*any", handel)
+	api.PATCH("/*any", handel)
+}
+
+func handel(c *gin.Context) {
 	path := c.Request.URL.Path
-	path = php2go.StrReplace("/api/v1/","",path,9999999999)
-	routePackage := php2go.Explode("/",path)[0]
-	c.String(200, routePackage)
+	path = php2go.StrReplace("/api/v1/", "", path, 9999999999)
+	pathArr := php2go.Explode("/", path)
+	routePackage := pathArr[0]
+	function := pathArr[1]
+	//routePackage.function()
+	c.String(200, routePackage+"/"+function)
 }
