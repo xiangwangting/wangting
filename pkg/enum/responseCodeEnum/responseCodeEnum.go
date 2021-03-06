d40d9066c10a51dab1f931e3dfbc8c129d118694
“xiangwang” <344496982@qq.com>
2021-03-06 20:13:36 +0800

    tijiao

--- pkg/enum/responseCodeEnum/responseCodeEnum.go
+++ pkg/enum/responseCodeEnum/responseCodeEnum.go
@@ -3,7 +3,7 @@ package responseCodeEnum
 //1000以下为通用码，1000以上为用户自定义码
 const (
 	SuccessCode             = 200  //成功
-	UndefErrorCode          = 400  //为定义的错误
+	UndefErrorCode          = 400  //未定义的错误
 	InternalErrorCode       = 500  //内部错误
 	InvalidRequestErrorCode = 401  //无效请求
 	CustomizeCode           = 1000 //业务自定义错误码
