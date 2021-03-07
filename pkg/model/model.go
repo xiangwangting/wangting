8fc4da312939d92558ac38cb97282819f68d360a
“xiangwang” <344496982@qq.com>
2021-03-07 22:54:18 +0800

    tijiao

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"gorm.io/gorm"
 	"time"
 )
 
@@ -8,9 +9,18 @@ import (
 //    type User struct {
 //      gorm.Model
 //    }
+//type Model struct {
+//
+//	Id        uint       `json:"id" gorm:"column:id"`
+//	UpdatedAt time.Time `json:"update_at" gorm:"column:update_at"" description:"更新时间"`
+//	CreatedAt time.Time `json:"create_at" gorm:"column:create_at" description:"创建时间"`
+//	DeletedAt time.Time `json:"delete_at" gorm:"column:delete_at" description:"删除时间"`
+//}
+
+// gorm.Model 的定义
 type Model struct {
-	Id        uint       `json:"id" gorm:"column:id"`
-	UpdatedAt time.Time `json:"update_at" gorm:"column:update_at"" description:"更新时间"`
-	CreatedAt time.Time `json:"create_at" gorm:"column:create_at" description:"创建时间"`
-	DeletedAt time.Time `json:"delete_at" gorm:"column:delete_at" description:"删除时间"`
+	ID        uint           `gorm:"primaryKey"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
