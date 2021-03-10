4ec4fee4f286a83bc2dce5b3e59b27f57d908408
“xiangwang” <344496982@qq.com>
2021-03-10 09:47:37 +0800

    路由demo

--- http/route/DemoRoute.go
+++ http/route/DemoRoute.go
@@ -9,9 +9,9 @@ import (
 func (h *Helper) DemoUserRoute() (r *Router) {
 	return &Router{
 		Path:   "/index",                //路由路径
-		Method: http.MethodGet,              //方法
+		Method: http.MethodGet,              //请求方式
 		Handlers: []gin.HandlerFunc{
-			demoHandel.Index,
+			demoHandel.Index,			//接口实现方法
 		},
 	}
 }
@@ -19,9 +19,9 @@ func (h *Helper) DemoUserRoute() (r *Router) {
 func (h *Helper) DemoErrorRoute() (r *Router) {
 	return &Router{
 		Path:   "/error",                //路由路径
-		Method: http.MethodGet,              //方法
+		Method: http.MethodGet,              //请求方式
 		Handlers: []gin.HandlerFunc{
-			demoHandel.DemoError,
+			demoHandel.DemoError, //接口实现方法
 		},
 	}
 }
@@ -29,9 +29,9 @@ func (h *Helper) DemoErrorRoute() (r *Router) {
 func (h *Helper) PostRoute() (r *Router) {
 	return &Router{
 		Path:   "/post",                //路由路径
-		Method: http.MethodPost,              //方法
+		Method: http.MethodPost,              //请求方式
 		Handlers: []gin.HandlerFunc{
-			demoHandel.Post,
+			demoHandel.Post, //接口实现方法
 		},
 	}
 }
@@ -41,7 +41,7 @@ func (h *Helper) GormRoute() (r *Router) {
 		Path:   "/gorm",                //路由路径
 		Method: http.MethodGet,              //方法
 		Handlers: []gin.HandlerFunc{
-			demoHandel.Gorm,
+			demoHandel.Gorm, //接口实现方法
 		},
 	}
 }
